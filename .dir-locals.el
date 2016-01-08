794e5cb97c22c103fc380dfb613efe0c7d871865
Chris Done <chrisdone@gmail.com>
2016-01-08 12:56:46 +0000

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((haskell-mode
+  . (
+     (haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng"))))
+ (haskell-cabal-mode
+  . ((haskell-process-type . ghci)
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng")))))
